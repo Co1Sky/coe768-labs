2e1015c739821e5775eb4094bc9639a55d118e74
Co1Sky <58276910+Co1Sky@users.noreply.github.com>
2023-10-06 08:58:15 -0400

    Update echo_client.c

--- lab2/echo_client.c
+++ lab2/echo_client.c
@@ -1,36 +1,38 @@
-/* A simple echo server using TCP */
+/* A simple echo client using TCP */
 #include <stdio.h>
-#include <string.h>
+#include <netdb.h>
 #include <sys/types.h>
-#include <sys/unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/signal.h>
-#include <sys/wait.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <strings.h>
+
 
 
 #define SERVER_TCP_PORT 3000	/* well-known port */
 #define BUFLEN		256	/* buffer length */
 
-int echod(int);
-void reaper(int);
-
 int main(int argc, char **argv)
 {
-	int 	sd, new_sd, client_len, port;
-	struct	sockaddr_in server, client;
+	int 	n, i, bytes_to_read;
+	int 	sd, port;
+	struct	hostent		*hp;
+	struct	sockaddr_in server;
+	char	*host, *bp, rbuf[BUFLEN], sbuf[BUFLEN];
 
 	switch(argc){
-	case 1:
+	case 2:
+		host = argv[1];
 		port = SERVER_TCP_PORT;
 		break;
-	case 2:
-		port = atoi(argv[1]);
+	case 3:
+		host = argv[1];
+		port = atoi(argv[2]);
 		break;
 	default:
-		fprintf(stderr, "Usage: %s [port]\n", argv[0]);
+		fprintf(stderr, "Usage: %s host [port]\n", argv[0]);
 		exit(1);
 	}
 
@@ -38,52 +40,27 @@ int main(int argc, char **argv)
 	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		fprintf(stderr, "Can't creat a socket\n");
 		exit(1);
-		exit(1);
 	}
 
-	/* Bind an address to the socket	*/
 	bzero((char *)&server, sizeof(struct sockaddr_in));
 	server.sin_family = AF_INET;
 	server.sin_port = htons(port);
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
-		fprintf(stderr, "Can't bind name to socket\n");
-		exit(1);
+	if (hp = gethostbyname(host)) 
+	  bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
+	else if ( inet_aton(host, (struct in_addr *) &server.sin_addr) ){
+	  fprintf(stderr, "Can't get server's address\n");
+	  exit(1);
 	}
 
-	/* queue up to 5 connect requests  */
-	listen(sd, 5);
-
-	(void) signal(SIGCHLD, reaper);
-
-	while(1) {
-	  client_len = sizeof(client);
-	  new_sd = accept(sd, (struct sockaddr *)&client, &client_len);
-	  if(new_sd < 0){
-	    fprintf(stderr, "Can't accept client \n");
-	    exit(1);
-	  }
-	  
-	  switch (fork()){
-	  case 0:		/* child */
-		(void) close(sd); 						// Close the Server Socket
-		char buf[BUFLEN] = "Hello!"; 
-		int buf_length = strlen(buf); 
-		write(new_sd, buf, buf_length); 
-		(void) close(new_sd); 					// Close Client Socket after saying "Hello!"
-		exit(0);
-	  default:		/* parent */
-		(void) close(new_sd);
-		break;
-	  case -1:
-		fprintf(stderr, "fork: error\n");
-	  }
+	/* Connecting to the server */
+	if (connect(sd, (struct sockaddr *)&server, sizeof(server)) == -1){
+	  fprintf(stderr, "Can't connect \n");
+	  exit(1);
 	}
-}
 
-/*	reaper		*/
-void	reaper(int sig)
-{
-	int	status;
-	while(wait3(&status, WNOHANG, (struct rusage *)0) >= 0);
+	read(sd, rbuf, BUFLEN); 
+	printf("%s \n", rbuf); 
+
+	close(sd);
+	return(0);
 }
